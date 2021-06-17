4474656ea1c59dad791db7ef65f2fd334df55919
EnockM1 <enockanes@gmail.com>
2021-06-17 08:11:41 -0700

    Tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/*Main function*/
+int main(void)
+{
+	char string[40];
+	
+	strcpy(str, "Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
+}
